f3f4b4dbe2768a6706758dbb3ccacde60a0c435b
sriegl <sriegl@uos.de>
2014-07-01 13:58:20 +0200

    pset08 50%

--- pset08/sriegl/magic_sequences.pl
+++ pset08/sriegl/magic_sequences.pl
@@ -31,10 +31,7 @@
 %-------------------------------------------------
 
 doit :-
-  writeln('############################################################'),
-  writeln('# max list length changed, revert back to 8 before release #'),
-  writeln('############################################################'),
-  between(0,5,N),
+  between(0,8,N),
   magic_sequence(N,S),
   writeln(N:S),
   fail.
@@ -49,13 +46,35 @@ doit.
 
 magic_sequence(Length, Sequence) :-
   length(Sequence, Length),
-  contains_ints(Sequence, Length),
-  check_items(Sequence, Sequence, 0).
+  contains_ints(Sequence, Length, Length),
+%  sum_list(Sequence, Length),
+%  check_items(Sequence, Sequence, 0).
+  check_items4(Sequence, Sequence, 0, Length).
 
-contains_ints([], _).
-contains_ints([SequenceHead|SequenceTail], Length) :-
-  between(0, Length, SequenceHead), !,
-  contains_ints(SequenceTail, Length).
+
+bam :-
+  between(0,8,N),
+  length(Sequence, N),
+  contains_ints(Sequence, N, N).
+  
+  
+contains_ints([], _, 0).
+contains_ints(Sequence, Length, AmountLeft) :-
+  append(FirstElements, [LastElement], Sequence), !,
+  between(0, AmountLeft, LastElement),
+  NewAmountLeft is AmountLeft - LastElement,
+  contains_ints(FirstElements, Length, NewAmountLeft).
+
+
+contains_ints2([], _, 0).
+contains_ints2([SequenceHead|SequenceTail], Length, AmountLeft) :-
+  between(0, AmountLeft, SequenceHead),
+  NewAmountLeft is AmountLeft - SequenceHead,
+  contains_ints(SequenceTail, Length, NewAmountLeft).
+
+
+%contains_ints3(Sequence, Length) :-
+  
 
 % Index: What to be counted.
 % CurrentAmount: Amount of what to be counted.
@@ -63,10 +82,53 @@ check_items([], _, _).
 check_items([CurrentAmount|ItemsLeft], Sequence, Index) :-
   count(Index, Sequence, CurrentAmount),
 %  writef('%w, %w, %w\n', [[CurrentAmount|ItemsLeft], Sequence, Index]),
-  OtherIndex is Index + 1, !,
-  check_items(ItemsLeft, Sequence, OtherIndex).
+  OtherIndex is Index + 1,
+  !, check_items(ItemsLeft, Sequence, OtherIndex).
 
 
+check_items1(_, [], _).
+check_items1(Sequence, WorkingList, Index) :-
+  nth0(Index, Sequence, Element),
+  delete(WorkingList, Element, NewWorkingList),
+  NewIndex is Index + 1, !,
+  check_items(Sequence, NewWorkingList, NewIndex).
+
+
+delete_n_times(List, _, 0, List).
+delete_n_times(List, Elem, N, ResultList) :-  
+  NMinusOne is N - 1,
+  select(Elem, List, ReducedList), !,
+  delete_n_times(ReducedList, Elem, NMinusOne, ResultList).
+
+
+%select_n(+Elem, +List1, -List2, ?N)
+select_n(_, [], [], 0).
+select_n(Elem, [Elem|List1Rest], List2, NPlusOne) :-
+  !, select_n(Elem, List1Rest, List2, N),
+  NPlusOne is N + 1.
+select_n(Elem, [OtherElem|List1Rest], [OtherElem|List2Rest], N) :-
+  OtherElem \= Elem,
+  !, select_n(Elem, List1Rest, List2Rest, N).
+ 
+ 
+check_items2(_, [], Length, Length).
+check_items2([SequenceHead|SequenceTail], WorkingList, Index, Length) :-
+  delete_n_times(WorkingList, Index, SequenceHead, ModifiedWorkingList),
+  IndexPlusOne is Index + 1, !,
+  check_items2(SequenceTail, ModifiedWorkingList, IndexPlusOne, Length).
+
+check_items3(_, [], Length, Length).
+check_items3([SequenceHead|SequenceTail], ItemsToCheck, Index, Length) :-
+  delete_all(ItemsToCheck, Index, RemainingItemsToCheck, SequenceHead),
+  IndexPlusOne is Index + 1,
+  !, check_items3(SequenceTail, RemainingItemsToCheck, IndexPlusOne, Length).
+
+
+check_items4(_, [], Length, Length).
+check_items4([SequenceHead|SequenceTail], ItemsToCheck, Index, Length) :-
+  select_n(Index, ItemsToCheck, RemainingItemsToCheck, SequenceHead),
+  IndexPlusOne is Index + 1,
+  !, check_items4(SequenceTail, RemainingItemsToCheck, IndexPlusOne, Length).
 
 %
 % count(+Needle, +Haystack, ?Amount)
@@ -85,5 +147,4 @@ count(Needle, [Needle|HaystackRest], Amount) :-
 count(Needle, [SomeHay|HaystackRest], Amount) :-
   Needle \= SomeHay, !,
   count(Needle, HaystackRest, Amount).
-
 
